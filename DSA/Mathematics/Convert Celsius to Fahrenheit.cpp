cf22d0abf7a8f1fb29ef758499b3966f8f264ccd
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-11-06 18:34:40 +0530

    Create Convert Celsius to Fahrenheit.cpp

---
+++ DSA/Mathematics/Convert Celsius to Fahrenheit.cpp
@@ -0,0 +1,11 @@
+class Solution{
+    public:
+    
+    //You need to complete this function
+    double cToF(int C)
+    {
+        //Your code here
+        double F= 1.8*C+32.0;
+        return F;
+    }
+};
