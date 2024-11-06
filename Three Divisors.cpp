1646d3abba18ed566f2bb81c1af5961cf4f9f491
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-11-06 23:31:27 +0530

    Create Three Divisors.cpp

---
+++ Three Divisors.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                ans++;
+            }
+        }
+        if(ans==3){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
