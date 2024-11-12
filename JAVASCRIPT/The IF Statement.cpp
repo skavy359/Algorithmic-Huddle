c3533c3ec2d57a33f554ecb2758d9d58ec21f6d3
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-11-12 12:58:24 +0530

    Create The IF Statement.cpp

---
+++ JAVASCRIPT/The IF Statement.cpp
@@ -0,0 +1,10 @@
+class Solution {
+    utility(number) {
+        if(number<100){
+            console.log("Number");
+        }
+        else{
+            console.log("Big\nNumber");
+        }
+    }
+}
