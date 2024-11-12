627ec28496712f8c3d7fd652dea546564c34fb2d
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-11-12 12:07:15 +0530

    Update and rename find-total-time-spent-by-each-employee.sql to Find Total Time Spent By Each Employee.sql

--- Find Total Time Spent By Each Employee.sql
+++ Find Total Time Spent By Each Employee.sql
@@ -1,5 +1,3 @@
-/* Write your PL/SQL query statement below */
-/* Write your PL/SQL query statement below */
 select to_char(event_day,'YYYY-MM-DD') as day, emp_id, sum(out_time-in_time) as total_time from Employees
 group by event_day,emp_id
 order by emp_id;
