33afb5442559b87fcf67b46c6bb2cbd72465295d
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-11-20 15:07:49 +0530

    Create Number of Good Pairs.cpp

---
+++ Number of Good Pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.size();j++){
+                if(i<j){
+                    if(nums[i]==nums[j]){
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
