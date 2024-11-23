e3699b2a397c40a8085bb5666c3e5fc0cd1b15b6
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-11-23 09:40:40 +0530

    Create Number of Steps to Reduce a Number to Zero.cpp

---
+++ Number of Steps to Reduce a Number to Zero.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int count=0;
+        while(num!=0){
+            if(num%2==0){
+                count++;
+                num=num/2;
+            }
+            else if(num%2!=0){
+                num=num-1;
+                count++;
+            }
+        }
+        return count;
+    }
+};
