3ff2d82876268ee8ed4f87714b77997874479521
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-11-28 11:08:23 +0530

    Create Mahmoud and Ehab and the Even Odd Game.cpp

---
+++ Mahmoud and Ehab and the Even Odd Game.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long a;
+    cin>>a;
+    
+    if(a%2==0){
+        cout<<"Mahmoud";
+    }
+    else{
+        cout<<"Ehab";
+    }
+    return 0;
+}
