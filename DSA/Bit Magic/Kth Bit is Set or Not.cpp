2d2e376c9807a4cfb6087f4f483d7a093e325381
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-12 13:14:18 +0530

    Delete DSA/Bit Magic directory

--- DSA/Bit Magic/Kth Bit is Set or Not.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-  public:
-    bool checkKthBit(int n, int k) {
-        return ((n>>k)&1); //for zero indexed yeh hoga 
-        // return ((n>>(k-1))&1) for one indexed 
-    }
-};
