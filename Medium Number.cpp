06e5fc0832fc538b9ee31eefd845b8b590902af1
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-14 13:53:38 +0530

    Create Medium Number.cpp

---
+++ Medium Number.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if ((a > b && a < c) || (a > c && a < b))
+        {
+            cout << a << endl;
+        }
+        else if ((b > a && b < c) || (b > c && b < a))
+        {
+            cout << b << endl;
+        }
+        else if ((c > a && c < b) || (c > b && c < a))
+        {
+            cout << c << endl;
+        }
+    }
+    return 0;
+}
