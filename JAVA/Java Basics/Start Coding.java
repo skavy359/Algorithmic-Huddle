24f4f64a91fce8dfeb51c88255b0958de74018bd
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-20 00:54:58 +0530

    Create Start Coding.java

---
+++ JAVA/Java Basics/Start Coding.java
@@ -0,0 +1,12 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+		system.out.println("Hello World");
+	}
+}
