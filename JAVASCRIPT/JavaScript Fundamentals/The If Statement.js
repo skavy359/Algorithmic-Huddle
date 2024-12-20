1493f4b2cd28c8a35653634b8dc8efc9970bbb97
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 02:29:18 +0530

    Create The If Statement.js

---
+++ JAVASCRIPT/JavaScript Fundamentals/The If Statement.js
@@ -0,0 +1,10 @@
+//User function Template for javascript
+class Solution {
+    utility(number) {
+        // write your if statement here
+        if(number>100){
+            console.log("Big");
+        }
+        console.log("Number");
+    }
+}
