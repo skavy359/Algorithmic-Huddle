e913b2901b14d2dc9c8c26b0b037d70f6c1541f2
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 14:14:45 +0530

    Create Arithmetic Operators.c

---
+++ C/Operators/Arithmetic Operators.c
@@ -0,0 +1,17 @@
+//User function Template for C
+
+void utility(int x, int y){
+     //Perform addition x+y below
+    int p = x+y;
+    //Perform subtraction x-y below
+    int q = x-y;
+    //Perform multiplication x*y below
+    int r = x*y;
+    //Perform division x/y below
+    int s =x/y;
+    //Perform modulo operation x%y below
+    int t = x%y;
+
+    //The below code prints the output. Don't change it!
+    printf("%d %d %d %d %d\n", p, q, r, s, t);
+}
