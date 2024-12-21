6c161e4e6a10d90e1a139734c409ccc9fbae2ee6
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 14:10:28 +0530

    Update and rename hi.txt to Geometric Progression Term.c

---
+++ C/Operators/Geometric Progression Term.c
@@ -0,0 +1,11 @@
+//User function Template for C
+#include <math.h>
+void utility(int a, int r, int n){
+    //Complete the code below
+    r=2;
+    int ans = a<<(n-1);
+    //Complete the code above
+    
+    //The line below prints the output. Don't change it!
+    printf("%d\n", ans);
+}
