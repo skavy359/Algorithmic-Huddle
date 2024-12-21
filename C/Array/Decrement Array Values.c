e6e100a9b41b9f594d803330cbc8b83cee9a0579
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 14:02:11 +0530

    Update and rename hi.txt to Decrement Array Values.c

---
+++ C/Array/Decrement Array Values.c
@@ -0,0 +1,11 @@
+//User function Template for C
+
+int * decrementArrayElements(int numbers[], int size){
+    for(int i=0;i<size;i++){
+        numbers[i]--;
+    }
+    return numbers;
+    //write your code here
+    
+    
+}
