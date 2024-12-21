cd94f321f295e726396808119a8f7bb732f1e047
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 13:54:59 +0530

    Create Print Without Space.c

---
+++ C/Input Output in C/Print Without Space.c
@@ -0,0 +1,8 @@
+//User function Template for C
+
+void utility(char a[], char b[]){
+    //Write your code below that prints ab 
+    printf("%s%s\n",a,b);
+    
+    //Write your code above that prints ab
+}
