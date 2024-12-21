a13ef6be8a63e6cd13233faf83d89055217b757a
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 23:28:26 +0530

    Create Right Angle Triangle.c

---
+++ C/Loops/Right Angle Triangle.c
@@ -0,0 +1,12 @@
+//User function Template for C
+
+void triangleWall(int s){
+    for(int i=1;i<=s;i++){
+        for(int j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    //Write your code here
+    
+}
