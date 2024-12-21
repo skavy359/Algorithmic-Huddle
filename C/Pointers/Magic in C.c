9da41b467db275a72dad069486bb2ca28b05d4e8
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 23:11:13 +0530

    Update and rename hi.txt to Magic in C.c

---
+++ C/Pointers/Magic in C.c
@@ -0,0 +1,12 @@
+//User function Template for C
+
+// Function to swap A and B
+// using pointers
+void swap(int *A, int *B){
+    int temp;
+    // Your code here
+    temp=*A;
+    *A=*B;
+    *B=temp;
+    
+}
