8d8a1f88dc7b70c6ea8fac3f7dcddf6075de4439
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 23:18:20 +0530

    Create String Concatenation.c

---
+++ C/Strings/String Concatenation.c
@@ -0,0 +1,11 @@
+//User function Template for C
+
+
+void concat(char *s1, char *s2)
+{
+    //Concatenate s1 and s2
+    strcat(s1,s2);
+    //Don't change below line
+    printf("%s\n", s1);
+}
+ 
