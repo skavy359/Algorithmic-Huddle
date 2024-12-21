d6144f217d8b507c3cd06c93fabaaf848657a1bc
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 14:07:37 +0530

    Create Sum All Array Elements.c

---
+++ C/Array/Sum All Array Elements.c
@@ -0,0 +1,14 @@
+//User function Template for C
+
+int arraySum(int numbers[], int size){
+    //Just return the sum of the list
+    //Don't print here
+    int sum=0;
+    for(int i=0;i<size;i++){
+        sum=sum+numbers[i];
+    }
+    return sum;
+    
+    
+    
+}
