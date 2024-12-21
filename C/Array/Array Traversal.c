fe989617e5669eae6f3fb891bc3f89fc8099a651
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 14:03:13 +0530

    Create Array Traversal.c

---
+++ C/Array/Array Traversal.c
@@ -0,0 +1,13 @@
+//User function Template for C
+
+void arrayTraversal(int numbers[], int size){
+    //Write your code below
+    int a;
+    
+    for(int i=0;i<size;i++){
+        printf("%d ",numbers[i]);
+    }
+    
+    
+    //Don't provide the new line
+}
