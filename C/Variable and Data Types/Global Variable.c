dfe19d921e109ee112a255b6ec0a03403845f449
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 13:50:11 +0530

    Create Global Variable.c

---
+++ C/Variable and Data Types/Global Variable.c
@@ -0,0 +1,17 @@
+//User function Template for C
+
+//decalre global variable g below
+int g=10;
+
+
+/**
+ * int getter(){
+     return g;
+ }
+ * 
+ * void setter(int x){
+     g = x;
+ }
+ * 
+
+*/
