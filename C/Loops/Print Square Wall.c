040f0b2146f3198778e6199988e6c90d79c5ae73
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 23:28:55 +0530

    Create Print Square Wall.c

---
+++ C/Loops/Print Square Wall.c
@@ -0,0 +1,12 @@
+//User function Template for C
+
+void squareWall(int s){
+    for(int i = 0; i < s; i++){
+        for(int j=0;j<s;j++){
+            printf("* ");
+        }
+        printf("\n");
+        //Write your code here
+        
+    }
+}
