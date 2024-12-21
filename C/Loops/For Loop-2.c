78ebb49d7793c9c809c1c487a20bd5cbcf2469be
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 23:30:48 +0530

    Create For Loop-2.c

---
+++ C/Loops/For Loop-2.c
@@ -0,0 +1,12 @@
+//User function Template for C
+
+void utility(char s[]){
+    // length of string is given by s.length()
+    
+    // character at any index i is given by s[i]
+    for(int i=0;i<=strlen(s);i=i+2){
+        printf("%c",s[i]);
+    };
+    //Write your code here
+    
+}
