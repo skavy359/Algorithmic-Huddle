b02236a85568f954573ee7935f48fec9a57914f2
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 14:08:01 +0530

    Create Count the Zeroes.c

---
+++ C/Array/Count the Zeroes.c
@@ -0,0 +1,12 @@
+//User function Template for C
+
+int countZeroes(int arr[], int n) {
+    // code here
+    int count=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            count+=1;
+        }
+    }
+    return count;
+}
