5445d00139d39643677de23bbe02dfb095f6ef12
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-21 14:26:10 +0530

    Create Function With Arguments.c

---
+++ C/Function/Function With Arguments.c
@@ -0,0 +1,16 @@
+//User function Template for C
+
+
+//Write the complete argumentFunction below.
+//The function should take two arguments a and b
+//The function should have only one statement
+void argumentFunction( int a, int b){
+    printf("%d\n",a+b);
+}
+
+
+
+
+//Write the complete argumentFunction below.
+//The function should take two arguments a and b
+//The function should have only one statement
