6728f02e845a7fe7ae5ed8da8306f0d249efe984
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-12-26 01:29:31 +0530

    Create Hit the Lottery.cpp

---
+++ Hit the Lottery.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    long long ans = 0;
+
+    ans += n / 100;
+    n %= 100;
+
+    ans += n / 20;
+    n %= 20;
+
+    ans += n / 10;
+    n %= 10;
+
+    ans += n / 5;
+    n %= 5;
+
+    ans += n;
+
+    cout << ans << endl;
+
+    return 0;
+}
