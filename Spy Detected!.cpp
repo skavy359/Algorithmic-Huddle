446affa9ac17aa66ff48a4ec919224e9f6d6d60b
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2025-01-01 00:45:21 +0530

    Create Spy Detected!.cpp

---
+++ Spy Detected!.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        if (arr[0] != arr[1] && arr[0] != arr[2])
+        {
+            cout << 1 << endl;
+        }
+        else
+        {
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[i] != arr[0])
+                {
+                    cout << i + 1 << endl;
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
