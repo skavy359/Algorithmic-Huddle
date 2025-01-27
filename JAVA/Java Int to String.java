b98deb51137d812650112758c27ce6761c365c42
Kavy Sharma <skavy359@gmail.com>
2025-01-27 22:21:22 +0530

    27 Jan Daily Question

--- JAVA/Java Int to String.java
+++ JAVA/Java Int to String.java
@@ -1,48 +1,48 @@
 import java.util.*;
 import java.security.*;
+
 public class Solution {
- public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  DoNotTerminate.forbidExit();
+        DoNotTerminate.forbidExit();
 
-  try {
-   Scanner in = new Scanner(System.in);
-   int n = in .nextInt();
-   in.close();
-   //String s=???; Complete this line below
+        try {
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            in.close();
+            // String s=???; Complete this line below
 
-   //Write your code here
-   String s= Integer.toString(n);
+            // Write your code here
+            String s = Integer.toString(n);
 
-   
-   if (n == Integer.parseInt(s)) {
-    System.out.println("Good job");
-   } else {
-    System.out.println("Wrong answer.");
-   }
-  } catch (DoNotTerminate.ExitTrappedException e) {
-   System.out.println("Unsuccessful Termination!!");
-  }
- }
+            if (n == Integer.parseInt(s)) {
+                System.out.println("Good job");
+            } else {
+                System.out.println("Wrong answer.");
+            }
+        } catch (DoNotTerminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }
 }
 
-//The following class will prevent you from terminating the code using exit(0)!
+// The following class will prevent you from terminating the code using exit(0)!
 class DoNotTerminate {
 
- public static class ExitTrappedException extends SecurityException {
+    public static class ExitTrappedException extends SecurityException {
 
-  private static final long serialVersionUID = 1;
- }
-
- public static void forbidExit() {
-  final SecurityManager securityManager = new SecurityManager() {
-   @Override
-   public void checkPermission(Permission permission) {
-    if (permission.getName().contains("exitVM")) {
-     throw new ExitTrappedException();
+        private static final long serialVersionUID = 1;
     }
-   }
-  };
-  System.setSecurityManager(securityManager);
- }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
 }
