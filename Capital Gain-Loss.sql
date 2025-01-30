a9e8e2bab7365babe19c3e678d637903be988b47
Kavy Sharma <skavy359@gmail.com>
2025-01-30 10:48:30 +0530

    Solved a Medium level sql problem which involved calculating net profit using sum aggregate functions

---
+++ Capital Gain-Loss.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select stock_name,
+sum(
+case when operation='Buy' then -price
+when operation='Sell' then +price
+end) as capital_gain_loss
+from Stocks
+group by stock_name;
