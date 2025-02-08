ce2bfa3aa18726ac310fae17628930fecc0aa5b1
Kavy Sharma <skavy359@gmail.com>
2025-02-09 01:17:24 +0530

    Solved a MySQL Database Problem

---
+++ Calculate Special Bonus.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select employee_id, case
+when name not like 'M%' and employee_id%2!=0 then salary
+else 0 
+end as bonus
+from employees
+order by employee_id;
