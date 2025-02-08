d6e4175310771a2bc73ba13466836067e0a72592
Kavy Sharma <skavy359@gmail.com>
2025-02-08 21:39:11 +0530

    Solved a Problem

---
+++ Minutes Before the New Year.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int h, m;
+        cin >> h >> m;
+
+        int total = (h * 60) + m;
+        cout << 1440 - total << endl;
+    }
+    return 0;
+}
