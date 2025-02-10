629275811d8f6fd6825c50384c0f4360ca1fb653
Kavy Sharma <skavy359@gmail.com>
2025-02-10 23:07:40 +0530

    POTD for 10th February

--- Clear Digits.cpp
+++ Clear Digits.cpp
@@ -1,16 +1,23 @@
-class Solution {
-    public:
-        string clearDigits(string s) {
-            string ans;
-            for (char c : s) {
-                if (isdigit(c)) {
-                    if (!ans.empty()) {
-                        ans.pop_back();
-                    }
-                } else {
-                    ans.push_back(c);
+class Solution
+{
+public:
+    string clearDigits(string s)
+    {
+        string ans;
+        for (char c : s)
+        {
+            if (isdigit(c))
+            {
+                if (!ans.empty())
+                {
+                    ans.pop_back();
                 }
             }
-            return ans;
+            else
+            {
+                ans.push_back(c);
+            }
         }
-    };
+        return ans;
+    }
+};
