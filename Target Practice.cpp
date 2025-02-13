660e64297fd68ad173588e13c9ab63a6426199e6
Kavy Sharma <skavy359@gmail.com>
2025-02-13 11:20:31 +0530

    Daily Problem-1

---
+++ Target Practice.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int scoreBoard[10][10] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+    {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+    {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+    {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+    {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+    {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+    {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        char arr[10][10];
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                cin >> arr[i][j];
+            }
+        }
+        long long ans = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if (arr[i][j] == 'X')
+                {
+                    ans += scoreBoard[i][j];
+                }
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
