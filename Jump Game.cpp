50d379950e3ada1b0be6ebd9950187bd1f0d7417
Kavy Sharma <skavy359@gmail.com>
2025-02-13 10:49:04 +0530

    Daily Problem

---
+++ Jump Game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        bool canJump(vector<int>& nums) {
+            int maximum = 0;
+            for (int i = 0; i < nums.size(); i++) {
+                if (maximum < i) {
+                    return false;
+                }
+                maximum = max(maximum, i + nums[i]);
+            }
+            return true;
+        }
+    };
