734658ed7157baaa01e656097521869949fe4b4a
Kavy Sharma <skavy359@gmail.com>
2025-03-11 23:21:48 +0530

    Solved Daily Problems

---
+++ Lunchtime.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+        if (x >= 1 && x <= 4)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
