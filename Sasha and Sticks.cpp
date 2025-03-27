b4a2028b69ba08241ca31bde42e41269b2dfd28d
Kavy Sharma <skavy359@gmail.com>
2025-03-27 17:05:01 +0530

    Solved Daily Problems and added missing files for previous few days and further to be added more

---
+++ Sasha and Sticks.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n, k;
+    cin >> n >> k;
+    if ((n / k) % 2 == 0)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+    return 0;
+}
