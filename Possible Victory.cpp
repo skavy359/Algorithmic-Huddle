87b3117bc933aefbb300233b5e5e84df6bad912e
Kavy Sharma <skavy359@gmail.com>
2025-04-01 22:59:05 +0530

    Solved Daily Problems

---
+++ Possible Victory.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int r, o, c;
+    cin >> r >> o >> c;
+    if (((20 - o) * 36) + c > r)
+    {
+        cout << "YES" << endl;
+    }
+    else
+        cout << "NO" << endl;
+    return 0;
+}
