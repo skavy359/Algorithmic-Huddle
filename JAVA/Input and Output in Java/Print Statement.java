023eefc9a941e3135dfc1f8020c81d5e39edc083
Kavy Sharma <skavy359@gmail.com>
2025-04-16 20:21:08 +0530

    Learnt about Input and Output in Java

---
+++ JAVA/Input and Output in Java/Print Statement.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        // Print S and move the cursor to a new line
+        System.out.println(s);
+    }
+}
