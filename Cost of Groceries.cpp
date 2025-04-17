2e7f2f66d76e1133156345166fa5217770078b53
Kavy Sharma <skavy359@gmail.com>
2025-04-17 09:36:37 +0530

    Solved Daily Problems

---
+++ Cost of Groceries.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+        int a[n], b[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        for (int j = 0; j < n; j++)
+        {
+            cin >> b[j];
+        }
+        // your code goes here
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] >= x)
+            {
+                sum += b[i];
+            }
+        }
+        cout << sum << endl;
+    }
+}
