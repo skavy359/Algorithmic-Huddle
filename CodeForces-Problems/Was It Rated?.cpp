a10e7afd1da1a7033e12dabcfeaca0d6e4143798
Kavy Sharma <skavy359@gmail.com>
2025-05-29 13:22:13 +0530

    Solved Codeforces and Leetcode Problems

---
+++ CodeForces-Problems/Was It Rated?.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    if (a == 15 || a == 20 || a == 21)
+        cout << "NO" << endl;
+    else
+        cout << "YES" << endl;
+    return 0;
+}
