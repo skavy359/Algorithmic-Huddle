caa9fb26416839efb8a85aebde3f5bb4ac663246
Kavy Sharma <skavy359@gmail.com>
2025-06-12 23:00:07 +0530

    Solved Codeforces and Leetcode Problems

--- Leetcode-Problems/2 Keys Keyboard.cpp
+++ Leetcode-Problems/2 Keys Keyboard.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int minSteps(int n) {
-        if (n == 1)
-            return 0;
+        if (n == 1) return 0;
         int count = 0;
         for (int i = 2; i <= n; i++) {
             while (n % i == 0) {
