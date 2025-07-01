18914a9015e6f2ba4acaa6f4ac2f1446c39cf993
Kavy Sharma <skavy359@gmail.com>
2025-07-02 01:05:31 +0530

    Solved Daily Leetcode Question

---
+++ Leetcode-Problems/Find the Original Typed String-I.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int possibleStringCount(string word)
+    {
+        int ans = 1;
+        for (int i = 0; i < word.length() - 1; i++)
+        {
+            if (word[i] == word[i + 1])
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
