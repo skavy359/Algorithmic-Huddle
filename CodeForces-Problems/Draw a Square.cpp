01672e990936451523d2cc70aaa2349edaca9875
Kavy Sharma <skavy359@gmail.com>
2025-07-21 14:40:30 +0530

    Solved Daily Leetcode Question and Codeforces Question

---
+++ CodeForces-Problems/Draw a Square.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        if (a == b && b == c && c == d)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+    return 0;
+}
