ac1daedbe41bc0b6626fc184fd75568d5a3f26cb
Kavy Sharma <skavy359@gmail.com>
2025-08-01 17:06:29 +0530

    Solved Daily Leetcode Questions and 4 more

---
+++ Leetcode-Problems/Transpose File.sh
@@ -0,0 +1,12 @@
+# Read from the file file.txt and print its transposed content to stdout.
+awk '
+{
+    for (i = 1; i <= NF; i++) {
+        a[i] = a[i] ? a[i] " " $i : $i
+    }
+}
+END {
+    for (i = 1; i <= length(a); i++) {
+        print a[i]
+    }
+}' file.txt
