0fd8ae9acf7cdcafc8fcee65ab2de4913cfbecbb
Kavy Sharma <skavy359@gmail.com>
2025-08-13 17:02:57 +0530

    Solved Daily Leetcode Question

---
+++ Leetcode-Problems/Split a String in Balanced Strings.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int balancedStringSplit(string s)
+    {
+        int ans = 0;
+        int balance = 0;
+        for (char c : s)
+        {
+            if (c == 'L')
+                balance++;
+            else
+                balance--;
+            if (balance == 0)
+                ans++;
+        }
+        return ans;
+    }
+};
