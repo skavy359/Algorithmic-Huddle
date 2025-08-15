63eb96ec0e73767fa121a6b097b4a7d5d847d014
Kavy Sharma <skavy359@gmail.com>
2025-08-15 12:59:30 +0530

    Solved Daily Leetcode Question

---
+++ Leetcode-Problems/Count Prefixes of a Given String.java
@@ -0,0 +1,12 @@
+class Solution {
+
+    public int countPrefixes(String[] words, String s) {
+        int ans = 0;
+        for (String word : words) {
+            if (s.startsWith(word)) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
