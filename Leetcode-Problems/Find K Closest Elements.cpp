8f20d84c9d24d08eed2653ba0b872691667665a0
Kavy Sharma <skavy359@gmail.com>
2025-09-06 02:23:46 +0530

    Solved Daily Leetcode Problem and Codeforces Problem

--- Leetcode-Problems/Find K Closest Elements.cpp
+++ Leetcode-Problems/Find K Closest Elements.cpp
@@ -19,4 +19,24 @@ public:
     }
 };
 
-//Another Solution
+// Another Solution
+class Solution
+{
+public:
+    vector<int> findClosestElements(vector<int> &arr, int k, int x)
+    {
+        int left = 0, right = arr.size() - 1;
+        while (right - left + 1 > k)
+        {
+            if (abs(arr[left] - x) > abs(arr[right] - x))
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return vector<int>(arr.begin() + left, arr.begin() + right + 1);
+    }
+};
