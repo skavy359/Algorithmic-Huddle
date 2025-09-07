de1f87841a1b39aeaf89bd5a243dc830f20eb62c
Kavy Sharma <skavy359@gmail.com>
2025-09-08 01:18:20 +0530

    Solved Daily Leetcode Problem and Codeforces Problem

---
+++ Leetcode-Problems/Complement of Base 10 Integer.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int bitwiseComplement(int n)
+    {
+        if (n == 0)
+            return 1;
+        int ans = 0, i = 0;
+        while (n > 0)
+        {
+            int bit = n & 1;
+            if (bit)
+                bit = 0;
+            else
+                bit = 1;
+            ans |= (bit << i);
+            i++;
+            n = n >> 1;
+        }
+        return ans;
+    }
+};
