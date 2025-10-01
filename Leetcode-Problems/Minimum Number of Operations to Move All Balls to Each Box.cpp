d11ec24e12bbec46d5b51adc356356f2482ac266
Kavy Sharma <skavy359@gmail.com>
2025-10-01 16:08:00 +0530

    Solved Daily Practice Problems

--- Leetcode-Problems/Minimum Number of Operations to Move All Balls to Each Box.cpp
+++ Leetcode-Problems/Minimum Number of Operations to Move All Balls to Each Box.cpp
@@ -19,3 +19,30 @@ public:
         return ans;
     }
 };
+
+// More Efficient Solution
+class Solution
+{
+public:
+    vector<int> minOperations(string boxes)
+    {
+        vector<int> ans(boxes.length(), 0);
+        int temp = 0, moves = 0;
+        for (int i = 0; i < boxes.length(); i++)
+        {
+            ans[i] += moves;
+            if (boxes[i] == '1')
+                temp++;
+            moves += temp;
+        }
+        temp = 0, moves = 0;
+        for (int i = boxes.length() - 1; i >= 0; i--)
+        {
+            ans[i] += moves;
+            if (boxes[i] == '1')
+                temp++;
+            moves += temp;
+        }
+        return ans;
+    }
+};
