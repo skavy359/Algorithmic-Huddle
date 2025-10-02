90421e6619d31b55faac5d5fc30b27a191b6cd0d
Kavy Sharma <skavy359@gmail.com>
2025-10-03 02:03:45 +0530

    Solved Daily Practice Problems

--- Leetcode-Problems/Delete Node in a Linked List.cpp
+++ Leetcode-Problems/Delete Node in a Linked List.cpp
@@ -11,7 +11,6 @@ class Solution
 public:
     void deleteNode(ListNode *node)
     {
-        ListNode *curr = node->next;
         node->val = node->next->val;
         node->next = node->next->next;
     }
