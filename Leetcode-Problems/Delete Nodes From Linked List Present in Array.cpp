463b5020e28a27ec2de00c7f1d0bbdc60be3ac40
Kavy Sharma <skavy359@gmail.com>
2025-10-03 02:01:03 +0530

    Solved Daily Practice Problems

---
+++ Leetcode-Problems/Delete Nodes From Linked List Present in Array.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *modifiedList(vector<int> &nums, ListNode *head)
+    {
+        unordered_set<int> st(nums.begin(), nums.end());
+        while (head != NULL && st.count(head->val))
+        {
+            head = head->next;
+        }
+        if (head == NULL)
+            return head;
+        ListNode *curr = head;
+        while (curr->next != NULL)
+        {
+            if (st.count(curr->next->val))
+            {
+                curr->next = curr->next->next;
+                continue;
+            }
+            curr = curr->next;
+        }
+        return head;
+    }
+};
