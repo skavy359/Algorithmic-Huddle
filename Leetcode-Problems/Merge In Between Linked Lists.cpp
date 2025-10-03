9e784b6ef4f015615a250d419eb8526f75d90f2f
Kavy Sharma <skavy359@gmail.com>
2025-10-03 17:26:54 +0530

    Solved Daily Practice Problems

---
+++ Leetcode-Problems/Merge In Between Linked Lists.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *mergeInBetween(ListNode *list1, int a, int b, ListNode *list2)
+    {
+        ListNode *A = list1;
+        ListNode *B = list1;
+        for (int i = 0; i < a - 1; i++)
+        {
+            A = A->next;
+        }
+        for (int i = 0; i < b; i++)
+        {
+            B = B->next;
+        }
+        A->next = list2;
+        while (list2->next != NULL)
+        {
+            list2 = list2->next;
+        }
+        list2->next = B->next;
+        return list1;
+    }
+};
