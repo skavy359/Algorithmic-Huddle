3498ce4329b7f001b919636bcd904205d68f0a02
Kavy Sharma <skavy359@gmail.com>
2025-12-08 12:45:45 +0530

    Solved Daily Practice Problem

---
+++ Leetcode-Problems/Count Square Sum Triples.cpp
@@ -0,0 +1,50 @@
+// BRUTE FORCE APPROACH
+//  class Solution
+//  {
+//  public:
+//      int countTriples(int n)
+//      {
+//          int count = 0;
+//          for (int i = 1; i <= n; i++)
+//          {
+//              for (int j = 1; j <= n; j++)
+//              {
+//                  for (int k = 1; k <= n; k++)
+//                  {
+//                      if (i * i + j * j == k * k)
+//                      {
+//                          count++;
+//                      }
+//                  }
+//              }
+//          }
+//          return count;
+//      }
+//  };
+
+// EFFICIENT APPROACH
+class Solution
+{
+public:
+    int countTriples(int n)
+    {
+        unordered_set<int> st;
+        for (int i = 1; i <= n; i++)
+        {
+            st.insert(i * i);
+        }
+        int khushi = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                int c = i * i + j * j;
+                if (st.count(c))
+                {
+                    khushi++;
+                }
+            }
+        }
+        return khushi;
+    }
+};
