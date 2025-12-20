879625352d8841c331d6229bc93c10ee9b289c20
Kavy Sharma <skavy359@gmail.com>
2025-12-20 23:32:53 +0530

    Solved Daily Practice Problems

---
+++ Leetcode-Problems/Minimum Number of Operations to Have Distinct Elements.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums)
+    {
+        unordered_set<int> st;
+        int ans = 0;
+        for (int i = nums.size() - 1; i >= 0; i--)
+        {
+            if (st.count(nums[i]))
+            {
+                ans = (i + 3) / 3;
+                break;
+            }
+            else
+                st.insert(nums[i]);
+        }
+        return ans;
+    }
+};
