086574419bcbbcaad3b653ae5adab409b92a752c
Kavy Sharma <skavy359@gmail.com>
2025-12-21 12:41:44 +0530

    Solved Daily Practice Problems

---
+++ Leetcode-Problems/Mirror Distance of an Integer.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int mirrorDistance(int n)
+    {
+        int temp = n, rev = 0;
+        while (temp > 0)
+        {
+            int rem = temp % 10;
+            rev = rev * 10 + rem;
+            temp /= 10;
+        }
+        return abs(n - rev);
+    }
+};
