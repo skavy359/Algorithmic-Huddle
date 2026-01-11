f5c0c33f87a7dd3300a64a8b971b29e679ae243b
Kavy Sharma <skavy359@gmail.com>
2026-01-11 12:12:26 +0530

    Solved Daily Practice Problems

---
+++ Leetcode-Problems/Minimum Moves to Reach Target Score.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int minMoves(int target, int maxDoubles)
+    {
+        int ans = 0;
+        while (target != 1)
+        {
+            if (target % 2 == 0 && maxDoubles > 0)
+            {
+                target /= 2;
+                maxDoubles--;
+            }
+            else
+            {
+                target--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
