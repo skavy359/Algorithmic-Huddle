459b3398893e3872a58d2934df98aa03db7b203e
Kavy Sharma <skavy359@gmail.com>
2026-01-13 19:41:19 +0530

    Solved Daily Practice Problems

---
+++ CodeForces-Problems/Social Experiment.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        if (n <= 3)
+        {
+            cout << n << "\n";
+        }
+        else if (n % 2 == 0)
+        {
+            cout << 0 << "\n";
+        }
+        else
+        {
+            cout << 1 << "\n";
+        }
+    }
+    return 0;
+}
