762b68fb7b4459065162ee5102a7ca605009be93
Kavy Sharma <skavy359@gmail.com>
2026-02-07 12:13:39 +0530

    Solved Daily Practice Problems

---
+++ Leetcode-Problems/Last Stone Weight.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i : stones)
+            pq.add(i);
+        while (pq.size() > 1) {
+            int x = pq.poll();
+            int y = pq.poll();
+            if (x == y) {
+                continue;
+            } else {
+                pq.add(x - y);
+            }
+        }
+        return pq.isEmpty() ? 0 : pq.peek();
+    }
+}
