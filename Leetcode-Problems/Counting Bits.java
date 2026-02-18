aa615fd7adcf32b46ae367c9d2c1c3d7ce1b3414
Kavy Sharma <skavy359@gmail.com>
2026-02-18 10:08:05 +0530

    Solved Daily Practice Problems

---
+++ Leetcode-Problems/Counting Bits.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            int temp = i;
+            while (temp > 0) {
+                ans[i] += (temp & 1);
+                temp >>= 1;
+            }
+        }
+        return ans;
+    }
+}
+
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1)
+        }
+        return ans;
+    }
+}
