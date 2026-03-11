2b7b1c7b705e55c77346fe1da79d2904264e3ae1
Kavy Sharma <skavy359@gmail.com>
2026-03-11 12:15:07 +0530

    Solved Daily Practice Problems

--- Leetcode-Problems/Complement of Base 10 Integer.java
+++ Leetcode-Problems/Complement of Base 10 Integer.java
@@ -1,22 +1,18 @@
-class Solution
-{
-public:
-    int findComplement(int n)
-    {
+class Solution {
+    public int bitwiseComplement(int n) {
         if (n == 0)
             return 1;
         int ans = 0, i = 0;
-        while (n > 0)
-        {
+        while (n > 0) {
             int bit = n & 1;
-            if (bit)
+            if (bit == 1)
                 bit = 0;
             else
                 bit = 1;
             ans |= (bit << i);
-            i++;
             n = n >> 1;
+            i++;
         }
         return ans;
     }
-};
+}
