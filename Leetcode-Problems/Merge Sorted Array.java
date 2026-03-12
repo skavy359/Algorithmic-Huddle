e848d5c2bea0f3bc95f8dcde5c1eac973e8eb074
Kavy Sharma <skavy359@gmail.com>
2026-03-12 22:40:35 +0530

    Solved Daily Practice Problems

--- Leetcode-Problems/Merge Sorted Array.java
+++ Leetcode-Problems/Merge Sorted Array.java
@@ -1,23 +1,35 @@
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         // for (int i = 0; i < n; i++) {
+//         //     nums1[m + i] = nums2[i];
+//         // }
+//         // Arrays.sort(nums1);  
+
+//         int i = m - 1, j = n - 1, k = m + n - 1;
+//         while (i >= 0 && j >= 0) {
+//             if (nums1[i] >= nums2[j]) {
+//                 nums1[k] = nums1[i];
+//                 i--;
+//             } else if (nums2[j] > nums1[i]) {
+//                 nums1[k] = nums2[j];
+//                 j--;
+//             }
+//             k--;
+//         }
+//         while (j >= 0) {
+//             nums1[k--] = nums2[j--];
+//         }
+//     }
+// }
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // for (int i = 0; i < n; i++) {
-        //     nums1[m + i] = nums2[i];
-        // }
-        // Arrays.sort(nums1);  
-
         int i = m - 1, j = n - 1, k = m + n - 1;
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] >= nums2[j]) {
-                nums1[k] = nums1[i];
-                i--;
-            } else if (nums2[j] > nums1[i]) {
-                nums1[k] = nums2[j];
-                j--;
-            }
-            k--;
-        }
         while (j >= 0) {
-            nums1[k--] = nums2[j--];
+            if (i >= 0 && nums1[i] >= nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else
+                nums1[k--] = nums2[j--];
         }
     }
 }
