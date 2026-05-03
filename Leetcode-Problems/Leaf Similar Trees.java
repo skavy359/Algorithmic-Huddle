cfc7ca2e404ec520f7a2378b479dd6289616d1fd
Kavy Sharma <skavy359@gmail.com>
2026-05-04 01:50:34 +0530

    Solved Daily Practice Problems

---
+++ Leetcode-Problems/Leaf Similar Trees.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> l1 = new ArrayList<>();
+    List<Integer> l2 = new ArrayList<>();
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        dfs(root1, l1);
+        dfs(root2, l2);
+        return l1.equals(l2);
+    }
+
+    void dfs(TreeNode root, List<Integer> list) {
+        if (root == null)
+            return;
+        if (root.left == null && root.right == null) {
+            list.add(root.val);
+            return;
+        }
+        dfs(root.left, list);
+        dfs(root.right, list);
+    }
+}
